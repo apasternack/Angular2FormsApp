b2b6910dc2e14b4d17f9ac2e94d3210ec9fa4d49
Adam Pasternack <adam.pasternack@gmail.com>
2017-03-22 13:44:55 -0400

    range custom validation

--- app/customers/customer.component.ts
+++ app/customers/customer.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function ratingRange(c: AbstractControl): {[key: string]: boolean} | null {
+        if (c.value !== undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
+            return { 'range': true };
+        };
+        return null;
+}
 
 @Component({
     selector: 'my-signup',
